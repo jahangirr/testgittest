3c4beed9f51aeb6f5222e51ecadaba103509bbe6
farhadnurtucky <tuckytushar@gmail.com>
2025-08-09 10:22:22 +0600

    new method is added

--- mychoice.js
+++ mychoice.js
@@ -8,3 +8,9 @@ function theirName()
 {
        return "then name ";
 }
+
+function mychoice() {
+    var name = myname();
+    var their = theirName();
+    return "My choice is: " + name + " and " + their;
+}
